f698a77ac87e1dc32f20207463563b7da27931df
Kirill P <kirill@webitmaster.ru>
2016-08-15 23:26:05 +0700

    end 5

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,35 +8,66 @@ def full_title(page_title)
     "#{base_title} | #{page_title}"
   end
 end
+
+describe ApplicationHelper do
+  describe 'full_title' do
+    it 'should include the page titie' do
+      expect(full_title('foo')).to match(/foo/)
+    end
+    it 'should include the base title' do
+      expect(full_title('foo')).to match(/^BASE/)
+    end
+    it 'should not include a bar for the home page' do
+      expect(full_title('')).not_to match(/\|/)
+    end
+  end
+end
+
 describe "Static pages" do
   let(:base_title) {"BASE"}
   subject {page}
+
+  shared_examples_for 'all static pages' do
+    it {should have_selector('h1', text: heading)}
+    it {should have_title(full_title(page_title))}
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About Us'))
+    click_link "Help"
+    expect(page).to have_title(full_title('Help'))
+    click_link "Contact"
+    expect(page).to have_title(full_title('Contact'))
+    click_link "Home"
+    click_link "Sign up now!"
+    expect(page).to have_title(full_title('Sign up'))
+    click_link "sample app"
+    expect(page).to have_title(full_title(''))
+  end
+
   describe "Home page" do
     before {visit root_path}
-
-    it {should have_content('Sample App')}
-    it {should have_title(full_title(''))}
-    it {should_not have_title('Home')}
+    let(:heading) {'sample app'}
+    let(:page_title) {''}
   end
 
   describe "Help page" do
     before {visit help_path}
-
-    it {should have_content('Help')}
-    it {should have_title(full_title('Help'))}
+    let(:heading) {'Help'}
+    let(:page_title) {'Help'}
   end
 
   describe "About page" do
     before {visit about_path}
+    let(:heading) {'About Us'}
+    let(:page_title) {'About'}
+  end
 
-    it {should have_content('About Us')}
-    it {should have_title(full_title('About'))}
-
-    describe "Contact page" do
-      before {visit contact_path}
-
-      it {should have_content('Contact')}
-      it {should have_title(full_title('Contact'))}
-    end
+  describe "Contact page" do
+    before {visit contact_path}
+    let(:heading) {'Contact'}
+    let(:page_title) {'Contact'}
   end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
+require 'spec_helper'
 
-RSpec.describe "UserPages",type: :controller do
-  subject {page}
-  RSpec.describe "signup page" do
-    before {visit signup_path}
+  describe "UserPages" do
+    subject {page}
+    describe "signup page" do
+      before {visit signup_path}
 
-    it {should have_content('Sign up')}
-    it {should have_title(full_title('BASE | Sign up'))}
-  end
+      it {should have_content('Sign up')}
+      it {should have_title(full_title('Sign up'))}
+    end
 end
